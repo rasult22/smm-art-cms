592eb5c69909737afb9869824caa26fddaba4e6f
Itachi-MacOS <warrior.006@inbox.ru>
2023-07-12 22:27:49 +0600

    update cors policy for image uploading from url

--- config/middlewares.js
+++ config/middlewares.js
@@ -26,7 +26,14 @@ module.exports = [
       },
     },
   },
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['http://localhost:1337', 'https://cms.smm.art', 'http://cms.smm.art']
+    }
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
